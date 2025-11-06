3c5d474fe7864102b0e434d3c1e45763a64ea271
konathalayashwanth <134399267+konathalayashwanth@users.noreply.github.com>
2025-11-06 13:00:41 +0530

    Time: 439 ms (15.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -0,0 +1,10 @@
+WITH temp AS(
+    SELECT DISTINCT student_id, subject, 
+    FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date) first_score,
+    LAST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) latest_score,
+    COUNT(student_id) OVER(PARTITION BY student_id, subject) diff_dates
+    FROM Scores
+)
+SELECT student_id,subject,first_score,latest_score
+ from temp
+where first_score<latest_score and diff_dates>1;
