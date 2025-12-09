03d25634ea4a65bcd934604a267f87fb3d2bfe3a
konathalayashwanth <134399267+konathalayashwanth@users.noreply.github.com>
2025-12-09 20:31:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (94.02%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hash_table=defaultdict(int)
+        result=0
         for num in nums:
-            hash_table[num]=hash_table[num]+1
-        for num in nums:
-            if hash_table[num]==1:
-                return num
+            result=result^num
+        return result
+  
 
 
 
