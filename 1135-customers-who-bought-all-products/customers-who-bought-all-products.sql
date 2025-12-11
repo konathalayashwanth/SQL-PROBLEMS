de633ee1cfb6824c36189e6b31a3909dd952fe19
konathalayashwanth <134399267+konathalayashwanth@users.noreply.github.com>
2025-12-11 21:15:41 +0530

    Time: 1377 ms (41.53%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,9 +1,6 @@
 
-with cte as
-(
-    select count(*) as cnt from product
-)
+
 select customer_id 
 from customer 
 group by customer_id
-having count(distinct product_key) =(select cnt from cte)
+having count(distinct product_key) =(select count(*) from product)
