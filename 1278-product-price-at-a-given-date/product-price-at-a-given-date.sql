46189ff39ce7762d9e5a9a6168318f52afa9642a
konathalayashwanth <134399267+konathalayashwanth@users.noreply.github.com>
2025-12-13 11:56:24 +0530

    Time: 833 ms (80.83%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,11 +1,11 @@
 /* Write your T-SQL query statement below */
-select product_id,
-first_value(new_price) over(partition by product_id order by change_date desc) as price
-from products
-where change_date<='2019-08-16'
-union
-select product_id,
-'10' as price
-from products
+select
+ product_id,
+ isnull((
+    select top 1 new_price
+    from Products p2
+    where p1.product_id = p2.product_id and change_date <= '2019-08-16'
+    order by change_date desc
+ ), 10) as price
+from Products p1
 group by product_id
-having min(change_date)>'2019-08-16'
