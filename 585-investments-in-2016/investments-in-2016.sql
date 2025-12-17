65228510b72813f8e0a0875983600a25a50ad562
konathalayashwanth <134399267+konathalayashwanth@users.noreply.github.com>
2025-12-17 15:14:20 +0530

    Time: 877 ms (7.46%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,24 +1,20 @@
-with tiv2015 as
-(
-select 
-tiv_2015
-from insurance
-group by tiv_2015
-having count(*)>1
-),
-lat_and_lon as
-(
-select 
-lat,
-lon
-from insurance
-group by lat,lon
-having count(*)=1
+/* Write your T-SQL query statement below */
+
+
+
+WITH CTE AS (
+		SELECT
+			*,
+			COUNT(lat) OVER(PARTITION BY lat,lon) CountLatLon,
+			COUNT(tiv_2015) OVER(PARTITION BY tiv_2015) CountT_2015
+		FROM
+			Insurance T1
 )
-select 
-round(sum(i.tiv_2016),2) as tiv_2016 
-from
-insurance i inner join tiv2015
-on i.tiv_2015 =tiv2015.tiv_2015 
-inner join lat_and_lon
-on i.lat=lat_and_lon.lat and i.lon=lat_and_lon.lon;
+SELECT
+	ROUND(SUM(tiv_2016),2) tiv_2016 
+FROM
+	CTE
+WHERE
+	CountLatLon = 1
+AND
+	CountT_2015 > 1
